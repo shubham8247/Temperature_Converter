2254ca07f2f1ddb90f34511d69dd2ef1fcef330d
Shubham Kumar <137132872+shubham8247@users.noreply.github.com>
2023-08-05 09:09:31 +0530

    Temperature_converter Commit
    I had created as temperature converter website using HTML,CSS &JavaScript

---
+++ index.js
@@ -0,0 +1,32 @@
+
+    var par = document.querySelector("#par");
+    let temprature = () => {
+        let opt = document.querySelector("#unit");
+        let display = document.querySelector(".value");
+        var a = document.querySelector(".takval").value;
+        if (opt.value == "Celsius") {
+            console.log(a, "C to F", opt.value)
+            let F = a * (9 / 5) + 32
+            display.innerHTML = F.toFixed(4) + " F"
+            // if (F < 32) {               
+            //     par.innerHTML = "Cool! at frezing point"
+            // } else if (F > 122) {              
+            //     par.innerHTML = "So Warm"
+            // } else {                
+            //     par.innerHTML = ""                
+            // }
+
+        } else {
+            console.log(a, "F to C", opt.value)
+            let C = (a - 32) * (5 / 9)
+            display.innerHTML = C.toFixed(4) + " ºC"
+            // if (C < 0) {
+            //     par.innerHTML = "Cool at frezing point"
+            // } else if (C > 50) {
+            //     par.innerHTML = "So Warm"
+            // } else {               
+            //     par.innerHTML = ""             
+
+            // }
+        }
+    }
